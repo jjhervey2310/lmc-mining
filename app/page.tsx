c9a61017b28526101fa76d1124d6a49b482f3d06
jjhervey2310 <jjhervey1@gmail.com>
2026-06-22 06:38:28 +0100

    Add source files for deployment

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>LMC Mining | Bitcoin Mining Intelligence Platform</h1>
+      <p>Independent Bitcoin mining data. Compare hardware, analyze hosting deals, calculate profitability.</p>
+    </main>
+  )
+}
