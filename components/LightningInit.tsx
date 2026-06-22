37262213b04f4e3c283e33930d92c8c8f32cfcf5
jjhervey2310 <jjhervey1@gmail.com>
2026-06-22 06:34:10 +0100

    Add source files for deployment

---
+++ components/LightningInit.tsx
@@ -0,0 +1,11 @@
+'use client'
+import { useEffect } from 'react'
+import { lightningEngine } from '@/lib/lightning-engine'
+
+export default function LightningInit() {
+  useEffect(() => {
+    lightningEngine.init()
+    // Never destroy – engine persists for the entire session
+  }, [])
+  return null
+}
